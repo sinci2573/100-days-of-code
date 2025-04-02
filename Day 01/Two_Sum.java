70a4bd672957e7c6674a8fc446db3c3dda376340
sinci2573 <sinci2573@gmail.com>
2025-04-02 13:35:48 +0530

    Update Two_Sum.java

--- Day 01/Two_Sum.java
+++ Day 01/Two_Sum.java
@@ -1,1 +1,22 @@
-
+import java.util.Scanner;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] nums=new Int();
+        int target;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            num[i]=s.nextInt();
+        }
+        target=s.nextInt();
+        for(i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if((num[i]+num[j])==target && num[i]!=num[j])
+                    System.out.print("["+i+","+j+"]");
+            }
+        }
+    }
+}
